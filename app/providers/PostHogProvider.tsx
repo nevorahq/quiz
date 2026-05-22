52f26cb728ade223b897bed4f2cca24fc244c8b9
Evgheni Nujnenco <nev.front.dev@gmail.com>
2026-05-22 21:27:39 +0300

    fixed variables

--- app/providers/PostHogProvider.tsx
+++ app/providers/PostHogProvider.tsx
@@ -24,8 +24,8 @@ function PostHogPageView() {
 }
 
 if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  posthog.init(process.env.POSTHOG_KEY!, {
+    api_host: process.env.POSTHOG_HOST,
     ui_host: 'https://eu.posthog.com',
     capture_pageview: false, // handled manually by PostHogPageView
     capture_pageleave: true,
